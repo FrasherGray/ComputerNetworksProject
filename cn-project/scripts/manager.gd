15fee7fa381a9c09d47d9f48308b368100ace416
FrasherGray <frasher@ohiogray.org>
2026-03-01 13:05:31 -0500

    Start UDP implementation

--- cn-project/scripts/manager.gd
+++ cn-project/scripts/manager.gd
@@ -1,7 +1,25 @@
 extends Node
+var UDPPacketHandler: PacketPeerStream = PacketPeerStream.new()
 @onready var timer_2: Timer = $Game/Ball/Timer2
 
-# UI functions
+
+# Multiplayer functions
+func _process(delta: float) -> void:
+	while UDPPacketHandler.get_available_packet_count() > 0:
+		var packet = UDPPacketHandler.get_packet()
+		if UDPPacketHandler.get_packet_error() != 0:
+			#discard packet, it's bad/wrong
+			continue
+		match packet[0]:
+			0: # other player moved paddle
+				if multiplayer.get_unique_id() == 1: # other player's paddle is the right one
+					get_node("Game/Right").global_position = Vector2(packet[1], packet[2])
+				else:
+					get_node("Game/Left").global_position = Vector2(packet[1], packet[2])
+			1: # ball bounced
+				pass
+
+# UI 
 func start_local_game() -> void:
 	get_node("Menu/Main").hide()
 	get_node("Game").show()
